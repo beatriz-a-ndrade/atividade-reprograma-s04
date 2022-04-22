860b9f5fad38626498d2f11fddef877b27af91a7
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-04-22 13:22:19 -0300

    Resolução exemplo3

---
+++ exemplo3.js
@@ -0,0 +1,10 @@
+// Exemplo 3 [tabuada] : Faça um programa que dado um número,
+// imprime no (console.log) a tabuada do mesmo de 1 a 10.
+
+let numero = 9
+
+for(let i = 1; i <= 10; i++) {
+    console.log(numero * i)
+}
+
+// for(começa em tanto; vai até tanto; contando de tanto em tanto)
